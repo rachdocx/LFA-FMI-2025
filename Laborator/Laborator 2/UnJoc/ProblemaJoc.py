f51602f812be56f1562d76d30222161a801c3526
gabrielrachieru <rachieru.gabriel2016@gmail.com>
2025-03-20 21:53:38 +0200

    .

--- Laborator/Laborator 2/UnJoc/ProblemaJoc.py
+++ Laborator/Laborator 2/UnJoc/ProblemaJoc.py
@@ -9,7 +9,25 @@ def states(a, state):
     for key in a.items():
         if key[0] == state:
             print(key[1])
-
+print("--------------Menu----------------")
+print("States are:")
+for x in data["states"]:
+    print(x)
+print("---------------------------------------------")
+print("Directions are:")
+for x in data["sigma"]:
+    print(x)
+print("---------------------------------------------")
+print("You're spawned at:")
+for x in data["start"]:
+    print(x)
+print("-----------------------------------------------")
+print("To win you need to reach:")
+for x in data["final"]:
+    print(x)
+print("--------------------------------------------------")
+print("Additional Rules:")
+print("You need the spoon to be able to Exit, Good Luck finding it!")
 inp=input()
 print(data)
 stare_actuala=data["start"][0]
@@ -24,6 +42,10 @@ for x in inp:
         if stare_actuala == "Kitchen":
             spoon = 1
 if stare_actuala==data["final"][0] and spoon==1:
-    print("ACCEPTAT")
+    print("Congratulations! You win!")
+elif stare_actuala==data["final"][0] and spoon==0:
+    print("Unfortunately, you don't have any spoon!")
+elif stare_actuala!=data["final"][0] and spoon==1:
+    print("Unfortunately, you didn't found the exit!")
 else:
-    print("NEACCEPTAT")
+    print("You lost!")
